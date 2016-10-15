468f5ee8478c979c2e745a0189983006a992f2e0
lpotasiewicz <lpotasiewicz@ccsuvt.org>
2016-10-14 21:49:21 -0400

    fixed my home

--- home.js
+++ home.js
@@ -1,1 +1,7 @@
-home.js
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}

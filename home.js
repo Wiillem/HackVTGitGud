365c6fe682e30d51249a5d15b84159d9f017501f
lpotasiewicz <lpotasiewicz@ccsuvt.org>
2016-10-15 00:42:54 -0400

    making stuffs

--- home.js
+++ home.js
@@ -2,4 +2,11 @@ var user = JSON.parse(localStorage.getItem("user"));
 console.log(user);
 document.getElementById("userName").innerHTML = user.name;
 document.getElementById("userRole").innerHTML = user.status;
-var classArr = [];
+var lessons = ['<li><a href="www.google.com">English: Creative Writing</a></li>',
+ '<li><a href="www.google.com">Mathematicis: Algebra for fun</a></li>',
+ '<li><a href="www.google.com">Science: Chemistry 200</a></li>'];
+var lessonsResults = document.getElementById("lessonsResults");
+lessonsResults.innerHTML = "";
+for (var i = 0; i < lessons.length; i++){
+	lessonsResults.innerHTML += lessons[i];
+}

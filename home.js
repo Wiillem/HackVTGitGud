e702694be8d594075c60b194fd6f3e0bd9ba6d06
lpotasiewicz <lpotasiewicz@ccsuvt.org>
2016-10-14 23:34:43 -0400

    lul

--- home.js
+++ home.js
@@ -2,3 +2,7 @@ var user = JSON.parse(localStorage.getItem("user"));
 console.log(user);
 document.getElementById("userName").innerHTML = user.name;
 document.getElementById("userRole").innerHTML = user.status;
+var classArr = [];
+fucntion displayLesson(){
+
+}

3dd56efa8cb04061ad624adc4a6e3f2ad485fe29
lpotasiewicz <lpotasiewicz@ccsuvt.org>
2016-10-14 21:32:58 -0400

    implementing main into login

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-var user;
-user.name = "from input box";
-user.status = "radial buttons teacher or student";
+var user = {};
+user.name = "";
+user.status = "";
 user.pic = '<img src="img/userImg.png">';
 user.lessons = [];
+
+function login(){
+	user.name = document.getElementById("userName").value;
+	if(document.getElementById("r1").checked){
+		user.status = "teacher";
+	} else {
+		user.status = "student";
+	}
+	console.log(user);
+}
 
 function addLesson(lesson){
 	user.lessons.push(lesson);
 }
 function removeLesson(lessonId){
-	for (var i = 0; i < user.lessons.length){
-		if (.user.lessons[i].id == lessonId){
+	for (var i = 0; i < user.lessons.length; i++){
+		if (user.lessons[i].id == lessonId){
 			user.lessons.splice(i, 1);
 		}
 	}
-} 
+}
